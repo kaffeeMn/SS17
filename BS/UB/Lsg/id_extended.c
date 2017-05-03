b3cbee0f887c0e759c1484e133ccc6548800d0c8
Maximilian Springenberg <spring02@fiws181.cs.tu-dortmund.de>
2017-05-03 14:49:50 +0200

    answers added

--- BS/UB/Lsg/id_extended.c
+++ BS/UB/Lsg/id_extended.c
@@ -5,8 +5,9 @@
 
 int main(int argc, char **argv){
     struct passwd *pwd;
-    pwd = getpwnam(argv[0]);
-    printf("%a user: %b\n", argv[1], pwd->pw_uid);
-    printf("%a group: %b\n", argv[1], pwd->pw_gid);
+    char *person = argv[1];
+    pwd = getpwnam(person);
+    printf("%s\tuser: %d\n", person, pwd->pw_uid);
+    printf("%s\tgroup: %d\n", person, pwd->pw_gid);
     return 0;
 }
