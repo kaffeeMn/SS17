f62d8e1f03747ab6fa9ea5f763e90143cc908690
max <mspringenberg@googlemail.com>
2017-05-09 22:50:28 +0200

    havn't compiled yet

--- DAP2/PR/Lsg/UB03/Sortieren.java
+++ DAP2/PR/Lsg/UB03/Sortieren.java
@@ -17,49 +17,65 @@ public class Sortieren{
             }
         }
     }
-    private static void printRunTime(long runTime, int[] arr){
-        // printing the runTime, not much to say here
-        if(isSorted(arr))   System.out.println("Feld sortiert.\nZeit benoetigt:\t"+runTime+"ms");
-        else                System.out.println("Feld NICHT sortiert.\nZeit benoetigt:\t"+runTime+"ms");
-    }
-    private static boolean isSorted(int[] arr){
-        // check for an sorted array by comparing each field with its followup
-        int limit = arr.length;
-        assert (limit > 0) : "The array, needs to consist of at least 1 INT.";
-        for(int i = 0; i < limit-1; ++i){
-            if(arr[i] > arr[i+1]) return false;
-        }
-        return true;
-    }
-    private static int[] downArr(int range){
+//    private static boolean isSorted(int[] arr){
+//        // check for an sorted array by comparing each field with its followup
+//        int limit = arr.length;
+//        assert (limit > 0) : "The array, needs to consist of at least 1 INT.";
+//        for(int i = 0; i < limit-1; ++i){
+//            if(arr[i] > arr[i+1]) return false;
+//        }
+//        return true;
+//    }
+    private static void downArr(int range, int[] result){
         // initializes an descending array
-        int[] result = new int[range];
+        result = new int[range];
         for(int i=range-1; i>0; --i)  result[range-(i+1)] = i;
-        return result;
     }
     /* Aufgabe 2
      * */
-    private static void handler(float time, int[] arr, int[]tmpArr){
+    private static void handler(float time, int[] arr, tmpLen){
         float tStart, tEnd, runTime;
         // beginning of the runTime
         tStart = System.currentTimeMillis();
         bubbleSort(arr);
         // end of runTime
         tEnd = System.currentTimeMillis();
-        runTime = tEnd-tStart;
-        if(Math.abs(time-runTime) <= 0.1f){
-            tmpArr = new int[arr.length];
-            tmpArr = arr;
-            arr = new int[arr.length*2];
-            arr = downArr(arr.length);
+        if(tEnd-tStart < time){
+            tmpLen = arr.length;
+            downArr(tmplen*2, arr);
             handler(time, arr, tmpArr);
         }else{
-            //TODO: implement ninary search
-            binarySearch(time, arr, tmpArr);
+            binarySearch(tmpLen, time, arr.length);
         }
     }
-    public void binarySearch(float time, int[]arr, int[]tmpArr){
-        //
+    public void binarySearch(float bottom , float wanted, float right){
+        float tStart, tEnd, runTime;
+        int[] arr;
+        downArr(right, arr);
+        // beginning of the runTime
+        tStart = System.currentTimeMillis();
+        bubbleSort(arr);
+        // end of runTime
+        tEnd = System.currentTimeMillis();
+        runTime = tEnd-tStart;
+        // Zwischenergebnis ausgeben lassen
+        System.out.println("Feldgroesse: "+right+"\nZeit: "+runTime);
+        if!((Math.abs(runTime-wanted) <= 0.1f)){
+            float q = (bottom+right)/2;
+            downArr(q, arr);
+            // beginning of the runTime
+            tStart = System.currentTimeMillis();
+            bubbleSort(arr);
+            // end of runTime
+            tEnd = System.currentTimeMillis();
+            runTime = tEnd-tStart;
+            if(wanted <= runTime) 
+                binarySearch(bottom, wanted, q);
+            else
+                binarySearch(bottom, ++q, right);
+        }else{
+            System.out.println("done");
+        }
     }
     public static void main(String[] args){
         int limit = args.length;
@@ -67,12 +83,12 @@ public class Sortieren{
         else{
             try{
                 float time = Float.parseFloat(args[0]);
-                int[] arr =  downArr(ARRNNUM);
-                int[] tmpArr = arr;
-                handler(time, arr, tmpArr);
+                int[] arr;
+                downArr(ARRNNUM, arr);
+                handler(time, arr, arr.length);
             }catch(Exception e){
-                e.printStacktrace()
-                System.out.println("EXCEPTION: " + e.getMessage())
+                e.printStacktrace();
+                System.out.println("EXCEPTION: " + e.getMessage());
             }
         }
     }
