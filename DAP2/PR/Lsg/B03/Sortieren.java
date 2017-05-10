cfcfd69e664969d1fd51f6bee7db9e30a114e5a2
Maximilian Springenberg <spring02@fiws179.cs.tu-dortmund.de>
2017-05-10 18:37:44 +0200

    finished

--- DAP2/PR/Lsg/B03/Sortieren.java
+++ DAP2/PR/Lsg/B03/Sortieren.java
@@ -1,12 +1,11 @@
 // TODO: System.gc() verwenden ...
 public class Sortieren{
-
     /* Aufgabe 1
      * */
     private static void bubbleSort(int[] arr){
         int n = arr.length;
         for(int i=0; i<n; ++i){
-            for(int j=n; j>i; --j){
+            for(int j=n-1; j>=i+1; --j){
                 if(arr[j-1]>arr[j]){
                     int tmp = arr[j];
                     arr[j] = arr[j-1];
@@ -15,73 +14,69 @@ public class Sortieren{
             }
         }
     }
-//    private static boolean isSorted(int[] arr){
-//        // check for an sorted array by comparing each field with its followup
-//        int limit = arr.length;
-//        assert (limit > 0) : "The array, needs to consist of at least 1 INT.";
-//        for(int i = 0; i < limit-1; ++i){
-//            if(arr[i] > arr[i+1]) return false;
-//        }
-//        return true;
-//    }
-    private static void downArr(int range, int[] result){
+    private static int[] downArr(int range){
         // initializes an descending array
-        result = new int[range];
+        int[] result = new int[range];
         for(int i=range-1; i>0; --i)  result[range-(i+1)] = i;
+        return result;
     }
     /* Aufgabe 2
      * */
     private static void handler(float time, int[] arr, int tmpLen){
-        float tStart, tEnd, runTime;
+        long tStart, tEnd;
         // beginning of the runTime
         tStart = System.currentTimeMillis();
         bubbleSort(arr);
         // end of runTime
         tEnd = System.currentTimeMillis();
-        if(tEnd-tStart < time){
+        float runTime = (tEnd-tStart) / 1000;
+        if(runTime < time){
             tmpLen = arr.length;
-            downArr(tmpLen*2, arr);
+            arr = downArr(tmpLen*2);
+            System.out.println("Feldgroesse: " + tmpLen);
             handler(time, arr, tmpLen);
         }else{
-            binarySearch(tmpLen, time, arr.length, arr);
+            System.out.println("\nentering binary search");
+            binarySearch(tmpLen, (tmpLen+arr.length)/2, arr.length, arr, time);
         }
     }
-    public static void binarySearch(int bottom , float wanted, int right, int[] arr){
-        float tStart, tEnd, runTime;
-        downArr(right, arr);
+    public static void binarySearch(int bottom, int mid, int right, int[] arr, float wanted){
+        long tStart, tEnd;
+        arr = downArr(mid);
         // beginning of the runTime
         tStart = System.currentTimeMillis();
         bubbleSort(arr);
         // end of runTime
         tEnd = System.currentTimeMillis();
-        runTime = tEnd-tStart;
+        float runTime = (tEnd-tStart)/1000;
         // Zwischenergebnis ausgeben lassen
-        System.out.println("Feldgroesse: "+right+"\nZeit: "+runTime);
+        System.out.println("Feldgroesse: "+mid+"\nZeit: "+runTime+"s");
         if(!(Math.abs(runTime-wanted) <= 0.1f)){
-            int q = (bottom+right)/2;
-            downArr(q, arr);
-            // beginning of the runTime
-            tStart = System.currentTimeMillis();
-            bubbleSort(arr);
-            // end of runTime
-            tEnd = System.currentTimeMillis();
-            runTime = tEnd-tStart;
             if(wanted <= runTime) 
-                binarySearch(bottom, wanted, q, arr);
+                binarySearch(bottom, (bottom+mid)/2, mid, arr, wanted);
             else
-                binarySearch(bottom, ++q, right, arr);
+                binarySearch(mid, (mid+right)/2 , right, arr, wanted);
         }else{
-            System.out.println("done");
+            System.out.println("\ndone");
+            System.out.println("======================================================================");
         }
+    }
+    public static void printArr(int[] arr){
+        for(int i=0; i<arr.length; ++i){
+            System.out.print(" "+arr[i]);
+        }
+        System.out.println();
     }
     public static void main(String[] args){
         int limit = args.length;
-        if(limit!=1)    System.out.println("too many arguments");
-        else{
+        if(limit!=1){    
+            System.out.println("too many arguments");
+        }else{
             try{
                 float time = Float.parseFloat(args[0]);
-                int[] arr = new int[1];
-                downArr(1000, arr);
+                int[] arr = downArr(1000);
+                System.out.println("======================================================================");
+                System.out.println("initial time: " + time + "s\n\nentering presearch");
                 handler(time, arr, arr.length);
             }catch(Exception e){
                 e.printStackTrace();
@@ -90,3 +85,4 @@ public class Sortieren{
         }
     }
 }
+
