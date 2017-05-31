b5ef20d2a761be28bf1a3a8597225ca4aeafa96e
max <mspringenberg@googlemail.com>
2017-06-01 00:07:44 +0200

    ...

--- DAP2/PR/Lsg/B06/Anwendung.java
+++ DAP2/PR/Lsg/B06/Anwendung.java
@@ -1,33 +1,64 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Anwendung{
 
-    public static int[][] txtToInts(String path){
-        byte[] bytes = Files.readAllBytes(Paths.get(path))
-        String lines = new String(bytes);
-        lines = lines.split("\n");
+    public static int[][] txtToInts(String path) throws Exception{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        String file = new String(bytes, StandardCharsets.UTF_8);
+        String[] lines = file.split("\n");
         int[][] result = new int[lines.length][2];
         String[] tmp = new String[2];
         for(int i=0; i<lines.length; ++i){
             tmp = lines[i].split(",", 2);
-            result[i][0] = Integer.parseint(tmp[0]);
+            result[i][0] = Integer.parseInt(tmp[0]);
             result[i][0] = Integer.parseInt(tmp[1]);
         }
         return result;
     }
-    public static ArrayList<Interval> pathsToArrayList(){
-        for(String path : args){
-            int[][] ranges = txtToInts(path);
-            Interval[] ivList = new Interval[ranges.length];
-            for(int i=0; i<ranges.length; ++i){
-                ivList[i] = new Interval(ranges[i][0], ranges[i][1]);
+    public static LinkedList<ArrayList<Interval>> pathsToArrayLists(String[] args){
+        Interval[] ivList;
+        LinkedList<ArrayList<Interval>> aList = new LinkedList<ArrayList<Interval>>();
+        ArrayList<Interval> tmp;
+        for(int j=0; j<args.length; ++j){
+            try{
+                tmp = new ArrayList<Interval>();
+                int[][] ranges = txtToInts(args[j]);
+                for(int i=0; i<ranges.length; ++i){
+                    tmp.add(new Interval(ranges[i][0], ranges[i][1]));
+                }
+                aList.add(tmp);
+            }catch(Exception e){
+                handle(e);
             }
         }
-        return ArrayList<Interval>(ivList);
+        return aList;
     }
-    public static ArrayList<Interval> intervalScheduling(ArrayList<Interval>){}
+    public static void handle(Exception e){
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+    }
+    public static ArrayList<Interval> intervalScheduling(ArrayList<Interval> intervals){
+        int j = 0;
+        for(int i=0; i<intervals.size(); ++i){
+            //
+        }
+        return intervals;
+    }
+    public static void printArrayList(ArrayList<Interval> arr){
+        String result = "";
+        for(Interval iv : arr){
+            result += iv.toString() + "\n";
+        }
+        System.out.print(result);
+    }
     public static void main(String[] args){
-        //
+        LinkedList<ArrayList<Interval>> aList = pathsToArrayLists(args);
+        for(ArrayList<Interval> arr : aList){
+            printArrayList(intervalScheduling(arr));
+        }
     }
 }
