a60f7a5911e5e973378593362f76a1380bf0a5f6
Maximilian Springenberg <spring02@fiws200.cs.tu-dortmund.de>
2017-06-22 11:38:54 +0200

    done

--- DAP2/PR/Lsg/B09/Rucksack.java
+++ DAP2/PR/Lsg/B09/Rucksack.java
@@ -1,35 +1,69 @@
+import java.util.Random;
+import java.lang.Math;
+
 public class Rucksack{
    
-    private static int[][] algorithm(Ware[] wares, int g){
-        int len = wares.length;
-        int[][] result = new int[len][g];
+    private static int[][] algorithm(Ware[] wares, int g, int p){
+        int[][] result = new int[wares.length+1][g];
+        int count = 1;
         for(int i=0; i<g; ++i){
-            result[0][i] = 0;
+            result[0][i] = 2*p*count++;
         }
-        for(int i=0; i<len; ++i){
+        for(int i=1; i<result.length; ++i){
             for(int j=0; j<g; ++j){
-                result[i][j] = evaluate(i, j, result, g);
+                if(wares[i-1].getWeight() > j){
+                    result[i][j] = result[i-1][j];
+                }else{
+                    result[i][j] = min(
+                        result[i-1][j], 
+                        wares[i-1].getValue() + result[i-1][j-wares[i-1].getWeight()]
+                    );
+                }
             }
         }
         return result;
     }
-    private static int evaluate(int i, int j, int[][] matrix, int g){
-        //
-        return 0;
+    private static int min(int... vals){
+        int result = vals[0];
+        for(int i=1; i<vals.length; ++i){
+            if(vals[i-1] > vals[i]){
+                result = vals[i];
+            }
+        }
+        return result;
     }
-    private static Ware[] initWare(int p){
-        //
-        Ware w = new Ware(0,0);
-        Ware[] l = {w};
-        return l;
+    private static Ware[] initWare(int p, int n){
+        java.util.Random rand = new java.util.Random();
+        Ware[] result = new Ware[n];
+        for(int i=0; i<result.length; ++i){
+            result[i] = new Ware(
+                rand.nextInt((int)Math.round(p*1.2-p*0.8)) + (int)Math.round(p*0.8),
+                rand.nextInt(900) + 100
+            );
+        }
+        return result;
     }
     private static void task(int n, int p, int g){
-        Ware[] wares = initWare(p);
+        Ware[] wares = initWare(p, n);
         float tStart, tEnd;
         tStart  = System.currentTimeMillis();
-        int[][] matrix = algorithm(wares, g);
+        int[][] matrix = algorithm(wares, g, p);
         tEnd    = System.currentTimeMillis();
         System.out.format("took %1$s ms", tEnd - tStart);
+        printMatrix(matrix);
+    }
+    private static void printMatrix(int[][] matrix){
+        //String result = "";
+        //for(int i=0; i<matrix.length; ++i){
+        //    for(int j=0; j<matrix[i].length; ++j){
+        //        result += ", " + matrix[i][j];
+        //    }
+        //    result += "\n";
+        //}
+        System.out.format(
+            /*"Resulting matrix:\n%1$s\n\n*/"With result: %1$s\n",
+            /*result,*/ matrix[matrix.length-1][matrix[0].length-1]
+        );
     }
     public static void main(String[] args){
         if(args == null){
